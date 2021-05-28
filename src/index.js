b3162659a178bcd040fa93fa4fff453d74259e93
Dmitry Kishkanev <donbdimo@gmail.com>
2021-05-29 00:56:08 +0300

    setupParcel

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('привет');
+import './sass/main.scss';
+import menu from './menu.json';
+import menuCardsTpl from './templates/menu-cards.hbs';
+
+const menuContainer = document.querySelector('.js-menu');
+
+const menuMarkup = menuCardsTpl(menu);
+menuContainer.insertAdjacentHTML('beforeend', menuMarkup);
