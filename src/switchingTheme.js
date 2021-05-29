7d1511d155e8c4c2b45309d89a6e509d7e1df508
Dmitry Kishkanev <donbdimo@gmail.com>
2021-05-30 01:54:15 +0300

    switchingTheme

---
+++ src/switchingTheme.js
@@ -0,0 +1,10 @@
+const bodyEl = document.querySelector('body');
+const switchEl = document.querySelector('theme-switch__toggle');
+
+const Theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+  };
+
+  bodyEl.addEventListener('change', );
+  switchEl.addEventListener('change', );
